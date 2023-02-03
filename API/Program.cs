3f3df40dafcde813e4eef2d43cc72cdc9e9a80ee
levifabi <levi.fabi.sp@gmail.com>
2023-02-03 15:01:24 +0100

    #angular use https and bootstrap

--- API/Program.cs
+++ API/Program.cs
@@ -29,7 +29,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200") );
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200","http://localhost:4200") );
 
 app.MapControllers();
 
